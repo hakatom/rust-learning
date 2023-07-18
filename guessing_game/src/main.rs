bd55c8deaa7168ebc2f56ac8dec41cbda6dde514
hakatom <doron.vaida@gmail.com>
2023-07-18 08:16:02 +0000

    guessing game iteration

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,3 +1,13 @@
+use std::io;
+use rand::Rng;
 fn main() {
-    println!("Hello, world!");
+    println!("guess the number!");
+    println!("please input your guess.");
+    let mut guess = String::new();
+
+    let secret_number = rand::thread_rng().gen_range(1..=100);
+
+    println!("The secret number is: {secret_number}");
+    io::stdin().read_line(&mut guess).expect("failed to read line");
+    println!("you guessed: {guess}");
 }
