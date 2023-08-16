8a1e986f28d8b8a7116c5d6a0f3599d8a25d8d39
hakatom <doron.vaida@gmail.com>
2023-08-16 08:16:01 +0000

    tests

--- minigrep/src/lib.rs
+++ minigrep/src/lib.rs
@@ -5,6 +5,9 @@ use std::error::Error;
 pub fn run(config: Config) -> Result<(), Box<dyn Error>> {
     let contents = fs::read_to_string(config.filepath)?;
     println!("with text:\n{contents}");
+    for line in search(&config.query, &contents) {
+        println!("{line}");
+    }
     Ok(())
 }
 
@@ -21,5 +24,33 @@ impl Config {
         let query = args[1].clone();
         let filepath = args[2].clone();
         Ok(Config {query, filepath})
+    }
+}
+
+pub fn search<'a>(query: &str, contents: &'a str) -> Vec<&'a str> {
+    let mut results = Vec::new();
+
+    for line in contents.lines() {
+        if line.contains(query) {
+            results.push(line);
+        }
+    }
+
+    results
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn one_result() {
+        let query = "duct";
+        let contents = "\
+Rust:
+safe, fast, productive.
+Pick three.";
+
+        assert_eq!(vec!["safe, fast, productive."], search(query, contents));
     }
 }
